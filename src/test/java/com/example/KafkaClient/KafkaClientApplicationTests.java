5061991ae5348cc92ece1c18f87e4681bd8f23ba
Abhinav <ranaabhinav50@gmail.com>
2023-03-12 13:18:03 +0530

    added Testcase

--- src/test/java/com/example/KafkaClient/KafkaClientApplicationTests.java
+++ src/test/java/com/example/KafkaClient/KafkaClientApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.KafkaClient;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class KafkaClientApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("lemma");
+		assertTrue(true);
 	}
 
 }
